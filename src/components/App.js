662d7131aa8e6da75ce974e21c1d302289a3a39f
StuartCowley <voidstalker@live.co.uk>
2022-09-15 14:23:52 +0100

    remove initial boilerplate, add basic style for app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/app.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app__container">Lets look at some hooks!</div>
+      <div className="app__container">
+        <div className="app__container-title">useState</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
