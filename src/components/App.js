c8759d7d94173d0e5c18e27aad811afc1a41e1a1
StuartCowley <voidstalker@live.co.uk>
2022-09-15 20:13:30 +0100

    added useState element

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import '../styles/app.css';
+import { useState } from "react";
+import "../styles/app.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <div className="app">
       <div className="app__container">Lets look at some hooks!</div>
       <div className="app__container">
         <div className="app__container-title">useState</div>
+        <div>Current count: {count}</div>
+        <button onClick={handleClick}>click me!</button>
       </div>
     </div>
   );
