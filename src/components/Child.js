c886f2aab4bd9ba8e2c30a01c866346cec0fbedb
StuartCowley <voidstalker@live.co.uk>
2022-09-15 15:32:07 +0100

    Add child and grandchild components for use with useContext

---
+++ src/components/Child.js
@@ -0,0 +1,13 @@
+import "../styles/child.css";
+
+const Child = () => {
+  return (
+    <div className="child">
+      <div className="child__title">
+        Child component
+      </div>
+    </div>
+  );
+};
+
+export default Child;
---
+++ src/components/Grandchild.js
@@ -0,0 +1,13 @@
+import "../styles/grandchild.css";
+
+const Grandchild = () => {
+  return (
+    <div className="grandchild">
+      <div className="grandchild__title">
+        Grandchild component
+      </div>
+    </div>
+  );
+};
+
+export default Grandchild;
